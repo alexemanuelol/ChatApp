8462e33b79339abbb3683174eb6efcc51a6496c2
Alexemanuelol <Alexander.emanuelsson94@gmail.com>
2020-05-26 00:28:27 +0200

    Add first draft of input overflow handling

--- curses_test.py
+++ curses_test.py
@@ -9,14 +9,25 @@ screenHeight = screenSize[0]
 screenWidth = screenSize[1]
 
 textbox = []
+
 inputString = ""
+inputDisplay = ""
+
+def update_input_box():
+    """  """
+    if len(inputString) >= 5:#screenWidth-2:
+        inputDisplay = inputString[-5:]
+    else:
+        inputDisplay = inputString
+    screen.addstr(screenHeight-1, 0, "> " + inputDisplay)
 
 char = None
 while True:
     screen.clear()
     for index, line in enumerate(textbox):
         screen.addstr(index, 0, line)
-    screen.addstr(screenHeight-1, 0, "> " + inputString)
+    #screen.addstr(screenHeight-1, 0, "> " + inputString)
+    update_input_box()
     screen.refresh()
     char = screen.getch()
     if char == 27: # ESC
@@ -27,13 +38,19 @@ while True:
                 textbox.pop(0)
             textbox.append(inputString)
             inputString = ""
-    elif char == 8: # BACKSPACE
+    elif char == 8 or char == 127: # BACKSPACE
         if inputString != "":
             inputString = inputString[:-1]
     else:
-        if len(inputString) == screenWidth-6:
+        if len(inputString) == screenWidth-3:
             pass
         else:
             inputString += str(chr(char))
 
+
 curses.endwin()
+
+
+print(inputString)
+print("\n\n")
+print(inputDisplay)
