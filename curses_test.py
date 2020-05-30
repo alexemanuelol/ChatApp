7f4cd20d284e690facce5d3ee234d58e5c9b35bd
Alexander <Alexander.Emanuelsson94@gmail.com>
2020-05-31 00:57:54 +0200

    Finished client chat formating with curses

--- curses_test.py
+++ curses_test.py
@@ -11,7 +11,11 @@ class chat_client():
     def __init__(self):
         """  """
         self.screen = curses.initscr()
+        self.screen.keypad(True)
         curses.noecho()
+
+        self.screenHeightMin = 13
+        self.screenWidthMin = 30
 
         self.screenHeight, self.screenWidth = self.screen.getmaxyx()
         self.textboxHeight = self.screenHeight - 2
@@ -19,8 +23,16 @@ class chat_client():
 
         self.lineQueue = []
         self.lineQueueDisplay = []
+        self.scrollIndex = 0
 
         self.inputString = ""
+        self.displayStringPos = 0
+
+        self.cursorPos = 0
+        self.visualCursorPos = 0
+
+        self.visualLeftPos = 0
+        self.visualRightPos = 0
 
 
     def update_screen_size(self):
@@ -34,7 +46,7 @@ class chat_client():
         """  """
         displayString = ""
         if len(self.inputString) >= self.lineWidth:
-            displayString = self.inputString[-self.lineWidth:]
+            displayString = self.inputString[self.visualLeftPos:self.visualRightPos]
         else:
             displayString = self.inputString
         self.screen.addstr(self.screenHeight-1, 0, "> " + displayString)
@@ -44,12 +56,17 @@ class chat_client():
         """  """
         displayText = []
         if len(self.lineQueue) >= self.textboxHeight:
-            displayText = self.lineQueue[-self.textboxHeight:]
+            displayText = self.lineQueue[-self.textboxHeight + self.scrollIndex:][:self.textboxHeight]
         else:
             displayText = self.lineQueue
 
         for index, line in enumerate(displayText):
             self.screen.addstr(index, 0, line)
+
+
+    def update_visual_cursor(self):
+        """  """
+        self.screen.move(self.screenHeight-1, self.visualCursorPos+2)
 
 
     def update_refresh(self):
@@ -63,6 +80,7 @@ class chat_client():
         self.update_screen_size()
         self.update_textbox()
         self.update_input_string_display()
+        self.update_visual_cursor()
         self.update_refresh()
 
 
@@ -77,23 +95,78 @@ class chat_client():
 
     def run(self):
         """  """
-        while True:
-            self.update()
+        self.visualLeftPos = self.cursorPos - self.visualCursorPos
+        self.visualRightPos = (self.lineWidth - self.visualCursorPos) + self.cursorPos
+        self.update()
+
+        while self.screenWidth > self.screenWidthMin and self.screenHeight > self.screenHeightMin:
+
             char = self.screen.get_wch()
-            if char == "\x1b": # ESC
+
+            if char == "\x1b":                  # ESC KEY
                 break
-            elif char == "\x00":
+            elif char == 259:                   # ARROW UP KEY (Scroll up)
+                if len(self.lineQueue) + self.scrollIndex > self.textboxHeight:
+                    self.scrollIndex -= 1
+            elif char == 258:                   # ARROW DOWN KEY (Scroll down)
+                if self.scrollIndex != 0:
+                    self.scrollIndex += 1
+            elif char == 260:                   # ARROW LEFT KEY (Scroll left)
+                if self.visualCursorPos != 0:
+                    self.visualCursorPos -= 1
+                if self.cursorPos != 0:
+                    self.cursorPos -= 1
+            elif char == 261:                   # ARROW RIGHT KEY (Scroll right)
+                if self.visualCursorPos < len(self.inputString) and self.visualCursorPos != self.lineWidth:
+                    self.visualCursorPos += 1
+                if self.cursorPos < len(self.inputString):
+                    self.cursorPos += 1
+            elif char == "\x00":                # WINDOWS KEY
                 pass
-            elif char == curses.KEY_RESIZE:
+            elif char == curses.KEY_RESIZE:     # RESIZE EVENT
                 self.screenHeight, self.screenWidth = self.screen.getmaxyx()
-            elif char == "\n": # ENTER
+            elif char == 262:                   # HOME KEY
+                self.visualCursorPos = 0
+                self.cursorPos = 0
+            elif char == 358:                   # END KEY
+                if len(self.inputString) >= self.lineWidth:
+                    self.visualCursorPos = self.lineWidth
+                else:
+                    self.visualCursorPos = len(self.inputString)
+                self.cursorPos = len(self.inputString)
+            elif char == "\n":                  # ENTER KEY
                 self.append_to_line_queue()
-            elif char == "\x08": # BACKSPACE
-                self.inputString = self.inputString[:-1]
-            else:
-                self.inputString += char
+                self.scrollIndex = 0
+                self.visualCursorPos = 0
+                self.cursorPos = 0
+            elif char == 330:                   # DELETE KEY
+                self.inputString = self.inputString[:self.cursorPos] + self.inputString[self.cursorPos:][1:]
+
+                if len(self.inputString) >= self.lineWidth:
+                    if len(self.inputString) == (self.visualRightPos - 1) and self.visualCursorPos != self.lineWidth:
+                        self.visualCursorPos += 1
+            elif char == "\x08":                # BACKSPACE KEY
+                self.inputString = self.inputString[:self.cursorPos][:-1] + self.inputString[self.cursorPos:]
+
+                if self.visualCursorPos != 0 and len(self.inputString) <= self.lineWidth and self.visualLeftPos == 0:
+                    self.visualCursorPos -= 1
+                elif self.visualCursorPos != 0 and len(self.inputString) >= self.lineWidth and self.visualLeftPos == 0:
+                    self.visualCursorPos -= 1
+                if self.cursorPos != 0:
+                    self.cursorPos -= 1
+            else:                               # Append characters to self.inputString
+                self.inputString = self.inputString[:self.cursorPos] + str(char) + self.inputString[self.cursorPos:]
+                if self.visualCursorPos != self.lineWidth:
+                    self.visualCursorPos += 1
+                self.cursorPos += 1
+
+            self.visualLeftPos = self.cursorPos - self.visualCursorPos
+            self.visualRightPos = self.visualLeftPos + self.lineWidth
+
+            self.update()
 
         curses.endwin()
+
 
 
 if __name__ == "__main__":
